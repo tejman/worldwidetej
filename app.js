3534a091cae5d659bec297b50d927bef099e7c69
Teja Rudraraju <rudrarajut@gmail.com>
2014-02-26 21:03:32 -0700

    added under construction

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', frontControl.landing);
+app.get("/", frontControl.landing);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
