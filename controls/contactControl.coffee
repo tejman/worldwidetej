b23c1d463d05c0053dc336a3c24cd13e6d725158
Teja Rudraraju <rudrarajut@gmail.com>
2014-03-19 11:38:36 -0600

    add mongohq auth

--- controls/contactControl.coffee
+++ controls/contactControl.coffee
@@ -5,6 +5,7 @@ formatEmail = (formData)->
   email = {}
   email.to = "rudrarajut@gmail.com"
   email.from = formData["from"] 
+  console.log formData.who
   email.subject = formData.who.join("-")+":  "+formData.subject
   email.text = formData.text + "\n \n" + "NAME OF SENDER: " + formData.name
   return email
--- controls/contactControl.js
+++ controls/contactControl.js
@@ -2,7 +2,7 @@
 (function() {
   var formatEmail, sendgrid;
 
-  sendgrid = (require("sendgrid"))(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
+  sendgrid = (require("sendgrid"))(global.env.SENDGRID_USERNAME, global.env.SENDGRID_PASSWORD);
 
   formatEmail = function(formData) {
     var email;
